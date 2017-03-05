dd27be93a5af232f8992dbbd3430ee60beef679d
dweston <daz.weston@gmail.com>
2017-03-05 20:16:24 +0000

    add ability to get book by id to API

--- app.js
+++ app.js
@@ -13,11 +13,28 @@ var bookRouter = express.Router();
 
 bookRouter.route('/Books')
     .get(function(req, res) {
-        Book.find(function(err, books) {
+        // only filter on known properties
+        var query = {};
+        if (req.query.genre)
+        {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function(err, books) {
             if (err) {
                 res.status(500).send(err);
             } else {
                 res.json(books);
+            }
+        });
+    });
+
+bookRouter.route('/Books/:bookId')
+    .get(function(req, res) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(book);
             }
         });
     });
