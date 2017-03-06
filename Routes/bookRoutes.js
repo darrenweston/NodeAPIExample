bd4177e026443fe53b5c9017e17d812d48164327
dweston <daz.weston@gmail.com>
2017-03-06 10:25:35 +0000

    refactor to bookController

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -5,27 +5,10 @@ var routes = function(Book) {
 
     var bookRouter = express.Router();
 
+    var bookController = require('../controllers/bookController.js')(Book);
     bookRouter.route('/')
-        .post(function(req, res) {
-            var book = new Book(req.body);
-            book.save();
-            res.status(201).send(book); // 201: created
-        })
-        .get(function(req, res) {
-            // only filter on known properties
-            var query = {};
-            if (req.query.genre)
-            {
-                query.genre = req.query.genre;
-            }
-            Book.find(query, function(err, books) {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    res.json(books);
-                }
-            });
-        });
+        .post(bookController.post)
+        .get(bookController.get);
 
     bookRouter.use('/:bookId', function(req, res, next) {
         Book.findById(req.params.bookId, function(err, book) {
