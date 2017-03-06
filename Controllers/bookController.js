5e081c96fb486a4ca6aa497dea152e0e9e5e30c0
dweston <daz.weston@gmail.com>
2017-03-06 12:51:14 +0000

    add bookController passing test

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -2,8 +2,14 @@ var bookController = function (Book) {
     
     var post = function (req, res) {
         var book = new Book(req.body);
-        book.save();
-        res.status(201).send(book); // 201: created
+        if (!req.body.title) {
+            res.status(400); // 400: Bad Request
+            res.send('Title is required');
+        } else {
+            book.save();
+            res.status(201); // 201: Created
+            res.send(book); 
+        }
     };
 
     var get = function (req, res) {
@@ -14,7 +20,8 @@ var bookController = function (Book) {
         }
         Book.find(query, function (err, books) {
             if (err) {
-                res.status(500).send(err);
+                res.status(500); // 500: Internal Server Error 
+                res.send(err);
             } else {
                 res.json(books);
             }
