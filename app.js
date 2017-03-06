8977ece134039edc068a9a956ad20291c364d73a
dweston <daz.weston@gmail.com>
2017-03-06 14:55:06 +0000

    add integration test

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express'),
     mongoose = require('mongoose');
-    bodyParser = require('body-parser');
+bodyParser = require('body-parser');
 
-var db = mongoose.connect('mongodb://localhost/bookAPI');
 mongoose.Promise = global.Promise;
+
+var db;
+if (process.env.ENV === 'Test') {
+    db = mongoose.connect('mongodb://localhost/bookAPI_test');
+} else {
+    db = mongoose.connect('mongodb://localhost/bookAPI');
+}
 
 var app = express();
 
@@ -11,17 +17,19 @@ var Book = require('./models/bookModel');
 
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var bookRouter = require('./routes/bookroutes')(Book);
 
 app.use('/api/books', bookRouter);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Welcome to my API!');
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Running on PORT: ' + port);
 });
+
+module.exports = app;
